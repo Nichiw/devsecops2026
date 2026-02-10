a9f1ac33e64066fd720a19618017b9db518a3d64
Pedro Rocha Horchulhack <17484454+hrchlhck@users.noreply.github.com>
2026-02-10 12:49:14 +0000

    Adicionado github actions

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+API = FastAPI()
+TAREFAS = []
+
+class Tarefa(BaseModel):
+    id: int
+    titulo: str
+    data_criacao: datetime
+    finalizado: bool = False
+
+async def criar_tarefa(titulo: str):
+    id = len(TAREFAS)
+    tarefa_nova = Tarefa(id=id, titulo=titulo, data_criacao=datetime.now(), finalizado=False)
+
+    TAREFAS.append(tarefa_nova)
+
+    return {"mensagem": "OK"}
+
+async def listar_tarefas():
+    return TAREFAS
+
+API.add_api_route("/tarefas", listar_tarefas, methods=['GET'])
+API.add_api_route("/criar", criar_tarefa, methods=['POST'])
